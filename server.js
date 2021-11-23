5363a1e637bb490734cbb2441783111e6de13f8e
Angelina Gerdzhikov <angelina.gerdzhikov@gmail.com>
2021-11-23 14:16:58 -0500

    Added secure headers

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const db = require('./app/models/index');
 
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000"
+  origin: process.env.CORS_OPTIONS_ORIGIN
 };
 
+app.use(helmet());
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
